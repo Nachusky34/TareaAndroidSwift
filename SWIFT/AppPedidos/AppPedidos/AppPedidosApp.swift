4c560d0057db3aea097f22a8a3595fb9c67acab4
Usuario invitado <Guest@B213-12.local>
2025-02-13 16:00:15 +0100

    Creacion de proyectoSwift

---
+++ SWIFT/AppPedidos/AppPedidos/AppPedidosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppPedidosApp.swift
+//  AppPedidos
+//
+//  Created by Usuario invitado on 13/2/25.
+//
+
+import SwiftUI
+
+@main
+struct AppPedidosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
