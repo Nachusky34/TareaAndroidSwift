b2977526301e6edae68f76238fb13863a53c2377
Mario Seoane <m.seoane2005@gmail.com>
2025-02-17 01:36:49 +0100

    Fragmento Historial y Vistas de Tarjetas COMPLETADAS
    Diseño de los XML de cada tarjeta en su fragmento

--- Android/app/src/main/java/com/example/tareafinal/fragmentos/FragmentoHistorial.java
+++ Android/app/src/main/java/com/example/tareafinal/fragmentos/FragmentoHistorial.java
@@ -12,10 +12,10 @@ import com.example.tareafinal.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link FragmentoTienda#newInstance} factory method to
+ * Use the {@link FragmentoHistorial#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class FragmentoTienda extends Fragment {
+public class FragmentoHistorial extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +26,7 @@ public class FragmentoTienda extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public FragmentoTienda() {
+    public FragmentoHistorial() {
         // Required empty public constructor
     }
 
@@ -36,11 +36,11 @@ public class FragmentoTienda extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentoTienda.
+     * @return A new instance of fragment FragmentoHistorial.
      */
     // TODO: Rename and change types and number of parameters
-    public static FragmentoTienda newInstance(String param1, String param2) {
-        FragmentoTienda fragment = new FragmentoTienda();
+    public static FragmentoHistorial newInstance(String param1, String param2) {
+        FragmentoHistorial fragment = new FragmentoHistorial();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,7 +61,6 @@ public class FragmentoTienda extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragmento_tienda, container, false);
+        return inflater.inflate(R.layout.fragment_historial, container, false);
     }
-
 }
