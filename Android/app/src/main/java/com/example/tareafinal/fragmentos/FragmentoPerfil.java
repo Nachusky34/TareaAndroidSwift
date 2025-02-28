3efb1963c4ea5ce2cc7486dcaf87335f243a3d9c
Mario Seoane <m.seoane2005@gmail.com>
2025-02-28 21:01:13 +0100

    Perfil casi completo(solo queda almacenar img de cámara en Firebase)

--- Android/app/src/main/java/com/example/tareafinal/fragmentos/FragmentoPerfil.java
+++ Android/app/src/main/java/com/example/tareafinal/fragmentos/FragmentoPerfil.java
@@ -1,17 +1,37 @@
 package com.example.tareafinal.fragmentos;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.tareafinal.R;
 import com.example.tareafinal.db.Usuario;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.ByteArrayOutputStream;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,9 +44,41 @@ public class FragmentoPerfil extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
     private TextView username, email, postalcode;
+    private ImageView imagePerfil;
     private Switch newsletter;
     private Usuario usuario;
+    private String idUser;
+
+    private FirebaseDatabase database;
+    private DatabaseReference dbReferencePerfil;
+
+    FirebaseStorage storage;
+    StorageReference storageRef;
+    ByteArrayOutputStream outputStream;
+
+    ActivityResultLauncher<Intent> fotoLauncher =
+            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+                if (result.getResultCode() == RESULT_OK) {
+                    Intent data = result.getData();
+                    if (data != null) {
+                        // Obtener el Bitmap directamente desde el Intent
+                        Bundle extras = data.getExtras();
+                        if (extras != null) {
+                            Bitmap imageBitmap = (Bitmap) extras.get("data"); // Captura la miniatura
+                            if (imagePerfil != null) { // Verificar que no sea null
+                                imagePerfil.setImageBitmap(imageBitmap); // Muestra la imagen en el ImageView
+                                if (imageBitmap != null) {
+                                    subirImagenAFirebase(imageBitmap);
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    Toast.makeText(getContext(), "No se tomó ninguna foto.", Toast.LENGTH_SHORT).show();
+                }
+            });
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -55,23 +107,95 @@ public class FragmentoPerfil extends Fragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        View view = getView();
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_perfil, container, false);
+
         username = view.findViewById(R.id.tv_username);
         email = view.findViewById(R.id.tv_email);
         postalcode = view.findViewById(R.id.tv_postalCode);
         newsletter = view.findViewById(R.id.switch_newsletter);
-    }
+        imagePerfil = view.findViewById(R.id.iv_user);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+        View layoutHacerFoto = view.findViewById(R.id.layout_hacer_foto);
+        layoutHacerFoto.setOnClickListener(v -> hacerFoto(v));
+
+        database = FirebaseDatabase.getInstance("https://pcera-2b2f4-default-rtdb.europe-west1.firebasedatabase.app/");
+        dbReferencePerfil = database.getReference("usuarios");
 
         usuario = (Usuario) getArguments().getSerializable("usuario");
+        if (usuario != null) {
+            idUser = usuario.getId();
+        }
+
+        cargarDatosUsuario();
+
+        return view;
+    }
+
+    private void cargarDatosUsuario() {
+
+        dbReferencePerfil.orderByChild("id").equalTo(idUser).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    for (DataSnapshot userSnapshot : snapshot.getChildren()) {
+                        Usuario usuario = userSnapshot.getValue(Usuario.class);
+                        if (usuario != null) {
+                            username.setText(usuario.getUsername());
+                            email.setText(usuario.getEmail());
+                            postalcode.setText(usuario.getPostalCode());
+                            newsletter.setChecked(usuario.isNewsletter());
+
+                            cargarImagenDesdeDrawable(usuario.getFotoPerfil());
+                        }
+                    }
+                } else {
+                    Toast.makeText(getContext(), "Usuario no encontrado", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getContext(), "Error de base de datos: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void hacerFoto(View view){
+        Intent intentFoto = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        fotoLauncher.launch(intentFoto);
+    }
+
+    private void subirImagenAFirebase(Bitmap imageBitmap) {
+        // Generar el nombre de la imagen dinámicamente
+        String nombreImagen = "foto_perfil_" + idUser;
+
+        // Convertir Bitmap a bytes
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+        byte[] data = outputStream.toByteArray();
+
+        // Subir imagen a Firebase Storage
+        DatabaseReference userRef = dbReferencePerfil.child(idUser).child("fotoPerfil");
+        userRef.setValue(nombreImagen)
+                .addOnSuccessListener(aVoid -> Toast.makeText(getContext(), "Imagen alamcenada", Toast.LENGTH_SHORT).show())
+                .addOnFailureListener(e -> Toast.makeText(getContext(), "Error al guardar nombre", Toast.LENGTH_SHORT).show());
+    }
 
 
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil, container, false);
+    private void cargarImagenDesdeDrawable(String nombreImagen) {
+        Context context = getContext();
+        if (context != null) {
+            int imageResource = context.getResources().getIdentifier(nombreImagen, "drawable", context.getPackageName());
+
+            if (imageResource != 0) {
+                imagePerfil.setImageResource(imageResource);
+            } else {
+                // si no encuentra la imagen ponemos la de defecto
+                imagePerfil.setImageResource(R.drawable.icono_perfil_blanco);
+            }
+        }
     }
 }
